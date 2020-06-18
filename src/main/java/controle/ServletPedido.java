7e9fde28d94a331ed75833f9c3aa1aeee6015f6c
Luiz Felipe <luizfelipe.freire@gmail.com>
2020-06-18 08:26:19 -0300

    Enviar o Pedido Via Whatsapp

--- src/main/java/controle/ServletPedido.java
+++ src/main/java/controle/ServletPedido.java
@@ -1,6 +1,8 @@
 package controle;
 
 import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 
 import javax.servlet.ServletException;
@@ -18,31 +20,41 @@ import modelo.ListaProduto;
 import modelo.Pedido;
 
 @WebServlet(urlPatterns = "/servletPedido")
-public class ServletPedido extends HttpServlet{
+public class ServletPedido extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		// 1 - gravar o cliente no banco de dados
-		
+
 		ClienteDao daoCliente = new ClienteDao();
 		HttpSession sessao = req.getSession();
 		Cliente cliente = (Cliente) sessao.getAttribute("objCliente");
 		daoCliente.incluir(cliente);
-		
+
 		Pedido pedido = (Pedido) sessao.getAttribute("objPedido");
 		pedido.setCliente(cliente);
 		pedido.setDataPedido(LocalDate.now());
-		
+
 		PedidoDao daoPedido = new PedidoDao();
 		daoPedido.incluir(pedido);
-		
+
 		ListaProduto[] lsProduto = (ListaProduto[]) sessao.getAttribute("lsProduto");
 		ListaProdutoDao daoListaP = new ListaProdutoDao();
-		for(ListaProduto item : lsProduto) {
+		String lsItem = "";
+		for (ListaProduto item : lsProduto) {
 			item.setIdPedido(pedido.getId());
 			daoListaP.incluir(item);
+			lsItem += "\n"+item.getProduto().getNome()+"/"+item.getQuantidade()+"/"+item.getTotal();
 		}
-		
+		String fone = "5561985607460";
+		String msg = "Olá, me chamo "+cliente.getNome()+
+				"\nFiz o pedido de N° "+pedido.getId()+
+				"\nProduto/Qt/Total"+
+				lsItem+
+				"\n*Valor Final "+pedido.getTotal()+"*"+
+				"\nPara ser entregue em "+cliente.getEndereco()+" - "+cliente.getCidade();
+		msg = URLEncoder.encode(msg,StandardCharsets.UTF_8.toString());
+		resp.sendRedirect("https://api.whatsapp.com/send?phone="+fone+"&text="+msg);
 	}
-	
+
 }
